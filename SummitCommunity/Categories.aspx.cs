1a5eefa2c6ee975dea2edc2d67e3b77751cd16af
spv <stellavalcheva@yahoo.com>
2016-01-26 08:11:18 +0200

    Added Admin/AdminPage.aspx

--- SummitCommunity/Categories.aspx.cs
+++ SummitCommunity/Categories.aspx.cs
@@ -5,14 +5,24 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using SummitCommunity.Data;
+using SummitCommunity.Data.Contracts;
+using SummitCommunity.Data.Models;
+
 namespace SummitCommunity
 {
     public partial class Categories : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            ISummitCommunityData data = new SummitCommunityData();
+            var allCategories = data.Categories.All().ToList();
 
+            RepeaterCategories.DataSource = allCategories;
+            RepeaterCategories.DataBind();
         }
+
+
 
         
     }

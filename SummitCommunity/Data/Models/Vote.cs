0d949aac071baa00f7de650324c582a0e943c3ef
baretata <zlatkoatanasov92@gmail.com>
2016-01-28 12:26:49 +0200

    VoteControl ascx user control added.

---
+++ SummitCommunity/Data/Models/Vote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SummitCommunity.Data.Models
+{
+    public class Vote
+    {
+        [Key, Column(Order = 1)]
+        public int QuestionId { get; set; }
+
+        public virtual Question Question { get; set; }
+
+        [Key, Column(Order = 0)]
+        public string UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        [Required]
+        public int Value { get; set; }
+    }
+}
